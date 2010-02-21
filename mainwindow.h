a225aa59f1fd987d01ab53bfe71aaa0b54620b0c
rankor777 <rankor777@7439fa44-0c26-11df-b905-2f917c5cb467>
2010-02-21 21:41:46 +0000

    Updated 22.02.2010

---
+++ mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui/QMainWindow>
+#include <QTime>
+#include <QMessageBox>
+#include <QStateMachine>
+#include <QDesktopWidget>
+#include <QPropertyAnimation>
+#include <QTextCodec>
+
+#include "QOSCAR/qoscar.h"
+#include "notifywidget.h"
+
+namespace Ui
+{
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    public:
+        MainWindow(QWidget *parent = 0);
+        ~MainWindow();
+
+        void login();
+        void showNotify(const QString &sHeader, const QString &sText);
+
+    private:
+        Ui::MainWindow *ui;
+        QOSCAR *oscar;
+        NotifyWidget *notifyWidget;
+        QTime time;
+        QStateMachine machine;
+
+    private slots:
+        void on_searchBtn_clicked();
+        void on_reqInfoBtn_clicked();
+        void on_typeEndedBtn_clicked();
+        void on_typeStartedBtn_clicked();
+        void on_pushButton_2_clicked();
+        void on_pushButton_clicked();
+        void on_treeWidget_itemSelectionChanged();
+        void on_requestCLBtn_clicked();
+        void on_setInfoBtn_clicked();
+        void on_SendBtn_clicked();
+        void on_loginButton_clicked();
+        void onTimer();
+
+        // OSCAR
+        void onError(QErrorType errorType, QString errorString);
+        void onLogin(void);
+        void onMessage(QMessage);
+        void onLogOff(void);
+        void onCLHandled();
+        void onMetaInfo(const QSNInfo &info, const quint8 type);
+
+    signals:
+        void sShowNotify();
+};
+
+#endif // MAINWINDOW_H
